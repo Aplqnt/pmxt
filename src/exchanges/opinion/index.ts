1d4d28edd84f874c0c3281a1f1edcb5c7a69c454
Aplqnt <bossejg007@outlook.com>
2026-01-17 04:17:26 -0500

    Update index.ts

--- src/exchanges/opinion/index.ts
+++ src/exchanges/opinion/index.ts
@@ -1,18 +1,12 @@
-import { PredictionMarketExchange, MarketFilterParams, ExchangeCredentials } from '../../BaseExchange';
-import { UnifiedMarket, OrderBook } from '../../types';
+import { PredictionMarketExchange, MarketFilterParams, ExchangeCredentials } from '../BaseExchange';
+import { UnifiedMarket, OrderBook } from '../types';
 import { fetchMarkets } from './fetchMarkets';
 import { searchMarkets } from './searchMarkets';
 import { fetchOrderBook } from './fetchOrderBook';
-import { OpinionAuth } from './auth';
 
 export class OpinionTradeExchange extends PredictionMarketExchange {
-    private auth?: OpinionAuth;
-
     constructor(credentials?: ExchangeCredentials) {
         super(credentials);
-        if (credentials?.privateKey) {
-            this.auth = new OpinionAuth(credentials);
-        }
     }
 
     get name(): string {
