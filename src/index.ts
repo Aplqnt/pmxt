72d7eadeced8b2793e5f9bba057cbe55c0aee93c
Aplqnt <bossejg007@outlook.com>
2026-01-17 04:24:31 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
+// src/exchanges/index.ts
 export * from './BaseExchange';
 export * from './types';
-export * from './exchanges/polymarket';
-export * from './exchanges/kalshi';
+export * from './polymarket';
+export * from './kalshi';
+export * from './opinion'; // Export the new folder
 
-import { PolymarketExchange } from './exchanges/polymarket';
-import { KalshiExchange } from './exchanges/kalshi';
+import { PolymarketExchange } from './polymarket';
+import { KalshiExchange } from './kalshi';
+import { OpinionTradeExchange } from './opinion';
 
 const pmxt = {
     polymarket: PolymarketExchange,
     kalshi: KalshiExchange,
+    opiniontrade: OpinionTradeExchange,
     Polymarket: PolymarketExchange,
-    Kalshi: KalshiExchange
+    Kalshi: KalshiExchange,
+    OpinionTrade: OpinionTradeExchange
 };
 
 export const polymarket = PolymarketExchange;
 export const kalshi = KalshiExchange;
+export const opiniontrade = OpinionTradeExchange;
 
 export default pmxt;
